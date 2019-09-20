9dad0ad41f0c683012d4a79be248a19f7bada21c
Igor Marjanović <yogimaanz@yahoo.com>
2019-09-20 15:25:16 +0200

    Initial RegularSet method implementation

--- C++ STL and more/C++ STL and more/RegularSet.cpp
+++ C++ STL and more/C++ STL and more/RegularSet.cpp
@@ -2,6 +2,7 @@
 #include "RegularSet.h"
 #include <set>
 #include <algorithm>
+#include <iostream>
 
 RegularSet::RegularSet()
 {
@@ -9,17 +10,23 @@ RegularSet::RegularSet()
 
 void RegularSet::insertSet(const int &value)
 {
+	// calling STL insert method on container std::set<>
+	Set.insert(value);
+	std::cout << "Inserted value " << value << " into the set." << std::endl;
 }
 
 std::set<int>::iterator RegularSet::searchSet(const int &value) const
 {
-	return std::set<int>::iterator();
+	// calling STL find method on container std::set<>, result is an iterator which has to be checked before use
+	std::set<int>::iterator result = Set.find(value);
+	// iterator should be checked by using != std::set<>.end() and if true we can print the sucessful search
+	return result;
 }
-
-
 void RegularSet::printSet() const
 {
-
+	// printing out all elements with foreach using auto
+	for (auto value : Set)
+		std::cout << value << std::endl;
 }
 
 RegularSet::~RegularSet()
