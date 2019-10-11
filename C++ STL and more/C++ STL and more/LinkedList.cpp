c0504e4bb5f090c82bbb5bc3e6db8b5a5a7ae364
Igor Marjanović <yogimaanz@yahoo.com>
2019-10-11 17:51:54 +0200

    Add at element at end and start, print list

--- C++ STL and more/C++ STL and more/LinkedList.cpp
+++ C++ STL and more/C++ STL and more/LinkedList.cpp
@@ -1,13 +1,34 @@
 #include "pch.h"
 #include "LinkedList.h"
-
+#include <iostream>
+#include <cstdlib>
 
 LinkedList::LinkedList()
 {
+	head = NULL;
 }
 
 void LinkedList::AddNode(int value)
 {
+	struct node* newNode = new node();
+	//newNode will be the last element inside the list so it should point to null
+	newNode->next = NULL;
+	newNode->data = value;
+	//check if head exists and if so go to the end of the list then add a node
+	if (head != NULL)
+	{
+		struct node* current = head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = newNode;
+	}
+	else
+		// head doesnt exist so new elements becomes the first node and head points to it
+	{
+		head = newNode;
+	}
 }
 
 void LinkedList::AddNodeAT(int value, int index)
@@ -16,7 +37,13 @@ void LinkedList::AddNodeAT(int value, int index)
 
 void LinkedList::AddElementAtFront(int value)
 {
+	//New node we are adding is going to replace/ be added to the start of the list 
+	struct node* newNode = new node();
+	newNode->data = value;
+	newNode->next = head->next;
+	head = newNode;
 }
+
 
 void LinkedList::DeleteElement(int value)
 {
@@ -32,9 +59,17 @@ void LinkedList::ReverseList()
 
 void LinkedList::PrintList()
 {
+	// go through the list and print the current element
+	struct node* current = head;
+	while (current->next != NULL)
+	{
+		std::cout << current->data << std::endl;
+		current = current->next;
+	}
 }
 
 
 LinkedList::~LinkedList()
 {
+
 }
